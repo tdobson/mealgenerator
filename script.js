520f268f0f5826dbaf2ee98de9830e80f7566675
Clara Jordan <clara@jordan-maynard.org>
2013-11-14 13:22:40 +0000

    Meal generator
    For making random meals

---
+++ script.js
@@ -0,0 +1,50 @@
+var protein=[
+'sardines',
+'tuna',
+'lentils',
+'sausages',
+'chicken',
+'cheese',
+'kidney beans',
+'chickpeas',
+];
+
+var carbs=[
+'mashed potato',
+'chips',
+'pasta',
+'noodles',
+'rice',
+];
+
+var veg1=[
+'carrots',
+'peas',
+'tomatoes',
+'onion',
+'sultanas',
+'squash',
+'parsnips',
+'cabbage',
+];
+
+var veg2=[
+'carrots',
+'peas',
+'tomatoes',
+'onion',
+'sultanas',
+'squash',
+'parsnips',
+'cabbage',
+];
+
+var headline=["",""];
+function pickone(list) { return(list[Math.floor(Math.random()*list.length)]); }
+
+function mainCourse() {
+		headline[0]=pickone(protein) +", " +pickone(carbs) +", " +pickone(veg1)+ ", " +pickone(veg2);
+		document.getElementById('main').innerHTML = headline[0];
+}
+
+
